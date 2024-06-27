e604fe59056e89865e643127d1c59f3048799992
Alexander Ambuehl <alexander.ambuehl@bit.admin.ch>
2024-06-27 11:36:44 +0200

    add basic contextLoads assertion to satisfy sonar

--- src/test/java/ch/admin/bit/eid/verifier_management/VerifierManagementApplicationTests.java
+++ src/test/java/ch/admin/bit/eid/verifier_management/VerifierManagementApplicationTests.java
@@ -1,13 +1,21 @@
 package ch.admin.bit.eid.verifier_management;
 
+import ch.admin.bit.eid.verifier_management.controllers.VerifierManagementController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class VerifierManagementApplicationTests {
 
+	@Autowired
+	private VerifierManagementController verifierManagementController;
+
 	@Test
 	void contextLoads() {
+		assertThat(verifierManagementController).isNotNull();
 	}
 
 }

9673be7ebb585c32c66a7b751ed0912fe01a4f5c
Alexander Ambuehl <alexander.ambuehl@bit.admin.ch>
2024-07-01 16:39:27 +0200

    update entrypoint

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,14 @@ FROM bit-base-images-docker-hosted.nexus.bit.admin.ch/bit/eclipse-temurin:17-jre
 
 EXPOSE 8080
 
+COPY scripts/entrypoint.sh /app/
+
 ARG JAR_FILE=target/*.jar
 ADD ${JAR_FILE} app.jar
 
+RUN set -uxe && \
+    chmod g=u /app/entrypoint.sh &&\
+    chmod +x /app/entrypoint.s
 # ENV spring_profiles_active=docker
 
-ENTRYPOINT ["java","-jar","/app.jar"]
+ENTRYPOINT ["/app/entrypoint.sh","-jar","/app.jar"]
---
+++ scripts/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Add microservice CAs
+echo "Adding certificates to Java truststore:"
+if ls /certs-app/*.crt &> /dev/null; then
+    for f in $(ls /certs-app/*.crt); do
+        CERT=$f
+        CERT_ALIAS=$(basename $CERT .crt)
+        echo " => adding $CERT as $CERT_ALIAS to truststore"
+        $JAVA_HOME/bin/keytool -importcert -file $CERT -alias $CERT_ALIAS -cacerts -storepass changeit -noprompt -trustcacerts
+    done
+else
+    echo " => No certificates found, skipping"
+fi
+java -Duser.timezone=Europe/Zurich \
+-Dspring.config.location=classpath:application.yml \
+-Dfile.encoding=UTF-8 \
+-Dspring.profiles.active=${STAGE} \
+-Djavax.net.ssl.trustStore=/usr/local/openjdk/lib/security/cacerts \
+-Djavax.net.ssl.trustStorePassword=changeit \
+-Dhttp.proxyHost=${HTTP_PROXY} \
+-Dhttp.proxyPort=8080 \
+-Dhttps.proxyHost=${HTTPS_PROXY} \
+-Dhttps.proxyPort=8080 \
+-Dhttp.nonProxyHosts="${NO_PROXY}" \
+-jar $1

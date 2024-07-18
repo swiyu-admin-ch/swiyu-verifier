f80027f8ef171a434a5aeffd19e99e8cd27eca71
Edgar Zehnder <edgar.zehnder@bit.admin.ch>
2024-07-18 14:08:02 +0200

    [EID-1639] fix sonar findings

--- src/main/java/ch/admin/bit/eid/verifier_management/models/dto/PresentationDefinitionDto.java
+++ src/main/java/ch/admin/bit/eid/verifier_management/models/dto/PresentationDefinitionDto.java
@@ -1,6 +1,5 @@
 package ch.admin.bit.eid.verifier_management.models.dto;
 
-import ch.admin.bit.eid.verifier_management.models.validations.NullOrFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -11,7 +10,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
-import java.util.Map;
 
 @Getter
 @Setter

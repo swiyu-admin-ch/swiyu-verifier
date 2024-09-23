dcca39d340ea2a10550bb6446a9bd4278e782aef
Bruno Zimmermann <bruno.zimmermann@bit.admin.ch>
2024-09-23 12:08:34 +0200

    EID-1673 - Change local dockerfile to allow proxy settings to be injected

--- local.Dockerfile
+++ local.Dockerfile
@@ -1,8 +1,21 @@
-# Dockerfile used for local builds and github
 FROM eclipse-temurin:21
 
 RUN mkdir -p /app
 WORKDIR /app
-COPY ./target/*.jar /app/app.jar
 
-ENTRYPOINT ["java","-jar", "app.jar"]
+COPY scripts/entrypoint-local.sh /app/entrypoint.sh
+
+RUN chmod 766 $JAVA_HOME/lib/security/cacerts
+
+ARG JAR_FILE=target/*.jar
+ADD ${JAR_FILE} /app/app.jar
+
+RUN set -uxe && \
+    chmod g=u /app/entrypoint.sh &&\
+    chmod +x /app/entrypoint.sh
+
+WORKDIR /app
+
+USER 1001
+
+ENTRYPOINT ["/app/entrypoint.sh","app.jar"]
---
+++ scripts/entrypoint-local.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Reduced entrypoint script for local execution - does not mount vault
+
+# Add microservice CAs
+echo "Adding certificates to Java truststore:"
+CERT_FILES=$(ls /certs-app/*.c*rt)
+if [ "$CERT_FILES" ]; then
+    for CERT in $CERT_FILES; do
+        CERT_ALIAS=$(basename $CERT .crt)
+        echo " => adding $CERT as $CERT_ALIAS to truststore"
+        $JAVA_HOME/bin/keytool -importcert -file $CERT -alias $CERT_ALIAS -cacerts -storepass changeit -noprompt -trustcacerts
+    done
+else
+    echo " => No certificates found, skipping"
+fi
+
+java -Duser.timezone=Europe/Zurich \
+-Dfile.encoding=UTF-8 \
+-Dspring.profiles.active=${STAGE} \
+-Djavax.net.ssl.trustStore=/usr/local/openjdk/lib/security/cacerts \
+-Djavax.net.ssl.trustStorePassword=changeit \
+-Dhttp.proxyHost=${HTTP_PROXY} \
+-Dhttp.proxyPort=8080 \
+-Dhttps.proxyHost=${HTTPS_PROXY} \
+-Dhttps.proxyPort=8080 \
+-Dhttp.nonProxyHosts="${NO_PROXY}" \
+-jar $1

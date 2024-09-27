ed041f32a2d5cbd807dd2c6114f9093dd655efd6
Bruno Zimmermann <bruno.zimmermann@bit.admin.ch>
2024-09-27 09:46:49 +0200

    EID-1673 - Remove functions not used for verification

--- src/main/java/ch/admin/bit/eid/oid4vp/model/statuslist/TokenStatusListToken.java
+++ src/main/java/ch/admin/bit/eid/oid4vp/model/statuslist/TokenStatusListToken.java
@@ -5,8 +5,6 @@
 
 package ch.admin.bit.eid.oid4vp.model.statuslist;
 
-import ch.admin.bit.eid.oid4vp.exception.VerificationException;
-import ch.admin.bit.eid.oid4vp.model.enums.ResponseErrorCodeEnum;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,10 +12,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Base64;
-import java.util.Map;
 import java.util.zip.DataFormatException;
-import java.util.zip.Deflater;
-import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterOutputStream;
 
 /**
@@ -43,19 +38,8 @@ public class TokenStatusListToken {
     /**
      * zlib zipped & url encoded bytes containing the status information
      */
-    private byte[] statusList;
+    private final byte[] statusList;
 
-
-    /**
-     * Creates a new empty token status list
-     *
-     * @param bits             how many bits each status list entry shall have
-     * @param statusListLength the number of status list entries available
-     */
-    public TokenStatusListToken(int bits, int statusListLength) {
-        this.bits = bits;
-        statusList = new byte[statusListLength];
-    }
 
     /**
      * Load existing TokenStatusList Token
@@ -72,18 +56,6 @@ public class TokenStatusListToken {
         return new TokenStatusListToken(bits, decodeStatusList(lst));
     }
 
-    /**
-     * Claims to be put in the status_list property
-     *
-     * @return a claim set containing
-     */
-    public Map<String, Object> getStatusListClaims() {
-        return Map.of("bits", bits, "lst", encodeStatusList(statusList));
-    }
-
-    public String getStatusListData() {
-        return encodeStatusList(statusList);
-    }
 
     /**
      * Retrieves the status on the given index. Can contain multiple status being set. Eg 3 = revoked & suspended
@@ -100,96 +72,12 @@ public class TokenStatusListToken {
         // Drop all bits larger than our status
         var maskedByte = entryByte & mask;
         // Shift the status to the start of the byte so 1 = revoked, 2 = suspended, etc, also removes all bits smaller than our status
-        var vcStatus = maskedByte >> bitIndex;
-        return vcStatus;
+        return maskedByte >> bitIndex;
     }
 
-    /**
-     * Sets status bit to active
-     *
-     * @param idx    index of the status list entry
-     * @param status a status bit, one of 1,2,4,8
-     */
-    public void setStatus(int idx, int status) {
-        verifyStatusArgument(status);
-        byte entryByte = getStatusEntryByte(idx);
-        entryByte |= getBitPosition(idx, status);
-        setStatusEntryByte(idx, entryByte);
-    }
-
-    /**
-     * Sets status bit to inactive (0)
-     *
-     * @param idx    index of the status list entry
-     * @param status a status bit, one of 1,2,4,8
-     */
-    public void unsetStatus(int idx, int status) {
-        verifyStatusArgument(status);
-        byte entryByte = getStatusEntryByte(idx);
-        // Shift the bit to the correct position in the byte
-        entryByte &= ~getBitPosition(idx, status);
-        setStatusEntryByte(idx, entryByte);
-    }
-
-    public boolean canRevoke() {
-        return bits >= TokenStatusListBit.REVOKED.getBitNumber();
-    }
-
-    public boolean canSuspend() {
-        return bits >= TokenStatusListBit.SUSPENDED.getBitNumber();
-    }
-
-    /**
-     * Moves the status bit to the correct position in a byte
-     * eg. with bits=2 and index 1 will move a status of 1 to
-     * 0b00000100
-     *
-     * @param idx    index of the entry
-     * @param status a status bit
-     * @return the status bit moved to the position in a byte
-     */
-    private int getBitPosition(int idx, int status) {
-        return status << (idx * bits) % 8;
-    }
-
-    private void verifyStatusArgument(int status) {
-        if (bits < status) {
-            throw new IllegalArgumentException("Status can not exceed bits but was %d while expecting maximum of %d".formatted(status, bits));
-        }
-        if (status % 2 != 0 && status != 1) {
-            throw new IllegalArgumentException("Can only set one single status bit at a time");
-        }
-    }
 
     private byte getStatusEntryByte(int idx) {
         return statusList[idx * bits / 8];
-    }
-
-    /**
-     * Warning, this sets the whole byte, can therefore also affect neighbouring statuses if statusValue is incorrect
-     *
-     * @param idx
-     * @param statusValue the bytes from getStatusEntryByte but modified
-     */
-    private void setStatusEntryByte(int idx, byte statusValue) {
-        statusList[idx * bits / 8] = statusValue;
-    }
-
-    private static String encodeStatusList(byte[] statusList) {
-        // zipping the data
-        try {
-            var zlibOutput = new ByteArrayOutputStream();
-            var deflaterStream = new DeflaterOutputStream(zlibOutput, new Deflater(9));
-            deflaterStream.write(statusList);
-            deflaterStream.finish();
-            byte[] clippedZlibOutput = Arrays.copyOf(zlibOutput.toByteArray(), zlibOutput.size());
-            deflaterStream.close();
-            return Base64.getUrlEncoder().withoutPadding().encodeToString(clippedZlibOutput);
-        } catch (IOException e) {
-            log.error("Error occurred during zipping of Status List data", e);
-            // TODO Get management entity here or throw at sensible place?
-            throw VerificationException.credentialError(e, ResponseErrorCodeEnum.UNRESOLVABLE_STATUS_LIST, "Status List data can not be zipped", null);
-        }
     }
 
     /**

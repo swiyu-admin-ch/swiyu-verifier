0584c95cea638240a37c94d8b0d0470bd22581a2
Frithjof Hoppe <frithjof.hoppe@bit.admin.ch>
2024-11-21 16:32:49 +0100

    chore: Add url rewriting mappings EID-2540

--- src/main/java/ch/admin/bit/eid/oid4vp/config/ApplicationProperties.java
+++ src/main/java/ch/admin/bit/eid/oid4vp/config/ApplicationProperties.java
@@ -1,16 +1,24 @@
 package ch.admin.bit.eid.oid4vp.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.Map;
+
 @Validated
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "application")
 public class ApplicationProperties {
+
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @NotNull
     private String externalUrl;
@@ -29,8 +37,14 @@ public class ApplicationProperties {
 
     @NotNull
     private String signingKeyVerificationMethod;
-
     private String logoUri;
 
+    @PostConstruct
+    public void init() throws JsonProcessingException {
+        urlMappings = objectMapper.readValue(rawUrlMappings, new TypeReference<Map<String, String>>() {});
+    }
+
+    private String rawUrlMappings;
+    private Map<String, String> urlMappings;
 }
 

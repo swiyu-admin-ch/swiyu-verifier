6b6760deaae19b8724151c09e5cc0f76a6ed06cc
Frithjof Hoppe <frithjof.hoppe@bit.admin.ch>
2024-12-18 10:32:13 +0100

    fix: Increase clearing interval to 7 minutes EID-2902

--- src/main/java/ch/admin/bit/eid/verifier_management/services/SchedulerService.java
+++ src/main/java/ch/admin/bit/eid/verifier_management/services/SchedulerService.java
@@ -1,18 +1,22 @@
 package ch.admin.bit.eid.verifier_management.services;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class SchedulerService {
     private final ManagementService managementService;
 
     @Scheduled(initialDelay = 0, fixedDelayString = "${application.data-clear-interval}")
     @SchedulerLock(name = "expireOffers")
     public void removeExpiredManagements() {
+        log.info("Start scheduled removing of expired managements");
         managementService.removeExpiredManagements();
+        log.info("Finished scheduled removing of expired managements");
     }
 }

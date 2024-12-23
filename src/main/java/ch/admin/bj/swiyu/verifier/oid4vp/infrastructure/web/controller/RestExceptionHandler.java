c273c533b5a3af4728ad97a38ee7ea25002ec2c0
Georg Ludewig <georg.ludewig@bit.admin.ch>
2024-12-23 12:03:50 +0100

    chore: improved logging when a resource not was not found (not logged as error anymore)

--- src/main/java/ch/admin/bj/swiyu/verifier/oid4vp/infrastructure/web/controller/RestExceptionHandler.java
+++ src/main/java/ch/admin/bj/swiyu/verifier/oid4vp/infrastructure/web/controller/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package ch.admin.bj.swiyu.verifier.oid4vp.infrastructure.web.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -7,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import static java.util.Objects.nonNull;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
@@ -27,9 +29,15 @@ public class RestExceptionHandler {
         return new ResponseEntity<>(responseMessage, HttpStatus.BAD_REQUEST);
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoResourceFoundException.class)
+    public void handleNoResourceFoundException(HttpServletRequest r) {
+        log.debug("resource not found for uri {}", r.getRequestURL());
+    }
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(INTERNAL_SERVER_ERROR)
-    public void handleException(Exception e) {
-        log.error("Unhandled exception occured", e);
+    public void handleException(Exception e, HttpServletRequest r) {
+        log.error("Unhandled exception occured for uri {}", r.getRequestURL(), e);
     }
 }

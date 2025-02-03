c7a9418a386c6f1c4d6a11333a19ebc6a7479f51
Frithjof Hoppe <frithjof.hoppe@bit.admin.ch>
2025-02-03 16:07:26 +0100

    fix(EID-3193,EID-3191): Handle malformed stream exception

--- src/main/java/ch/admin/bj/swiyu/verifier/oid4vp/infrastructure/web/controller/RestExceptionHandler.java
+++ src/main/java/ch/admin/bj/swiyu/verifier/oid4vp/infrastructure/web/controller/RestExceptionHandler.java
@@ -4,12 +4,14 @@ import io.fabric8.kubernetes.client.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.MultipartStream;
 import org.springframework.beans.InvalidPropertyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
@@ -44,10 +46,20 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         if(ex.getMessage() != null && ex.getMessage().contains("Broken pipe")) {
             // This is most likely a wrapped client abort exception meaning the client has already disconnected
             // Because there's no point in returning a response null is returned
-            log.debug("Client aborted connection: {}", ex.getMessage());
+            log.debug("Client aborted connection", ex);
             return null;
         }
         log.error("Unhandled IO exception occurred", ex);
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MultipartException.class)
+    public ResponseEntity<Object> handleUnexpectedStreamClosing(MultipartException ex) {
+        if(ex.getMessage() != null && ex.getMessage().contains("Stream ended unexpectedly")) {
+            log.debug("Stream ended unexpectedly", ex);
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        log.error("Unhandled MultipartException exception occurred", ex);
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 

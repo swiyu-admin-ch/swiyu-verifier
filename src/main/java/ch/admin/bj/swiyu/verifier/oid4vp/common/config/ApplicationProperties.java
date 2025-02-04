2de54ef08f031a8ea2bcd0307297d552941b55eb
Alexander Ambühl <alexander.ambuehl@bit.admin.ch>
2025-02-04 11:48:04 +0100

    feat(EID-2173): review feedback einarbeiten

--- src/main/java/ch/admin/bj/swiyu/verifier/oid4vp/common/config/ApplicationProperties.java
+++ src/main/java/ch/admin/bj/swiyu/verifier/oid4vp/common/config/ApplicationProperties.java
@@ -35,7 +35,10 @@ public class ApplicationProperties {
     private HSMProperties hsm;
 
     @NotNull
-    private String requestObjectVersion;
+    private String requestObjectVersion = "1.0.0";
+
+    @NotNull
+    private String metadataVersion = "1.0";
 
 }
 

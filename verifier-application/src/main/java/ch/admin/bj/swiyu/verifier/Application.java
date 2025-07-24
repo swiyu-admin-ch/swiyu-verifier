4a2c9c6a10c14b3a49f026540af0fc71d06bcec9
zib <bruno.zimmermann@bit.admin.ch>
2025-07-24 10:47:03 +0200

    fix(EIDOMNI-174): Prevent spring from creating a user and printing the password to the terminal, causing worry and confusion

--- verifier-application/src/main/java/ch/admin/bj/swiyu/verifier/Application.java
+++ verifier-application/src/main/java/ch/admin/bj/swiyu/verifier/Application.java
@@ -10,12 +10,12 @@ import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan
 @Slf4j
@@ -29,7 +29,7 @@ public class Application {
         String serverPort = env.getProperty("server.port");
         log.info(
                 """
-                                                
+                        
                         ----------------------------------------------------------------------------
                         \t'{}' is running!\s
                         \tProfile(s): \t\t\t\t{}

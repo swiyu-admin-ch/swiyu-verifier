5b2639f89d7b8391733be375a8a9e35e5c487465
Edgar Zehnder <edgar.zehnder@bit.admin.ch>
2025-08-28 13:49:32 +0200

    chore(EIDOMNI-245) : update openapi to contain WebhookDto

--- verifier-application/src/main/java/ch/admin/bj/swiyu/verifier/infrastructure/config/OpenApiConfig.java
+++ verifier-application/src/main/java/ch/admin/bj/swiyu/verifier/infrastructure/config/OpenApiConfig.java
@@ -6,8 +6,11 @@
 
 package ch.admin.bj.swiyu.verifier.infrastructure.config;
 
+import ch.admin.bj.swiyu.verifier.api.callback.WebhookCallbackDto;
+import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.oas.models.OpenAPI;
 import lombok.AllArgsConstructor;
+import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,5 +35,11 @@ public class OpenApiConfig {
                 .group("API")
                 .pathsToMatch("/**")
                 .build();
+    }
+
+    @Bean
+    public GlobalOpenApiCustomizer openApiCustomizer() {
+        return openApi -> openApi.getComponents().getSchemas().put("WebhookCallback",
+                ModelConverters.getInstance().readAllAsResolvedSchema(WebhookCallbackDto.class).schema);
     }
 }

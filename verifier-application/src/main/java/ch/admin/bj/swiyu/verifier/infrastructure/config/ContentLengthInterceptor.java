d925db2398bee75a54f30a1024a2c7c3af8e03de
Edgar Zehnder <edgar.zehnder@bit.admin.ch>
2025-11-17 13:35:18 +0100

    feature(EIDOMNI-363) :  check status list size refuse if unknown

--- verifier-application/src/main/java/ch/admin/bj/swiyu/verifier/infrastructure/config/ContentLengthInterceptor.java
+++ verifier-application/src/main/java/ch/admin/bj/swiyu/verifier/infrastructure/config/ContentLengthInterceptor.java
@@ -7,10 +7,12 @@
 package ch.admin.bj.swiyu.verifier.infrastructure.config;
 
 import java.io.IOException;
+import java.net.URI;
 
 import ch.admin.bj.swiyu.verifier.service.statuslist.StatusListMaxSizeExceededException;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -21,6 +23,7 @@ public class ContentLengthInterceptor implements ClientHttpRequestInterceptor {
 
 
     private final int maxSize;
+    private static final String CHUNKED_TRANSFER_ENCODING = "chunked";
 
     @NotNull
     @Override
@@ -28,10 +31,25 @@ public class ContentLengthInterceptor implements ClientHttpRequestInterceptor {
         ClientHttpResponse response = execution.execute(request, body);
         long contentLength = response.getHeaders().getContentLength();
 
+        String transferEncoding = response.getHeaders().getFirst(HttpHeaders.TRANSFER_ENCODING);
+
+        // decline if transfer-encoding is chunked
+        if (CHUNKED_TRANSFER_ENCODING.equalsIgnoreCase(transferEncoding)) {
+            throw new IllegalArgumentException(getStatusListSizeUnknownMessage(request.getURI()) + " (chunked transfer encoding)");
+        }
+
+        if (contentLength == -1) {
+            throw new IllegalArgumentException(getStatusListSizeUnknownMessage(request.getURI()));
+        }
+
         if (contentLength > this.maxSize) {
             throw new StatusListMaxSizeExceededException("Status list size from %s exceeds maximum allowed size".formatted(request.getURI()));
         }
 
         return response;
+    }
+
+    private String getStatusListSizeUnknownMessage(URI uri) {
+        return "Status list size from %s could not be determined".formatted(uri);
     }
 }

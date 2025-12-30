eab20a2456989a9f2010b33f693d0c97dfc4a0d9
Edgar Zehnder <edgar.zehnder@bit.admin.ch>
2025-12-30 15:38:26 +0100

    fix(eidomni-363) : replace restclient with webclient

--- verifier-application/src/main/java/ch/admin/bj/swiyu/verifier/infrastructure/config/WebClientConfig.java
+++ verifier-application/src/main/java/ch/admin/bj/swiyu/verifier/infrastructure/config/WebClientConfig.java
@@ -10,20 +10,12 @@ import ch.admin.bj.swiyu.verifier.common.config.VerificationProperties;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @AllArgsConstructor
-public class RestClientConfig {
+public class WebClientConfig {
     private final VerificationProperties verificationProperties;
-
-    @Bean
-    public RestClient defaultRestClient(RestClient.Builder builder) {
-        return builder
-                .requestInterceptor(new ContentLengthInterceptor(verificationProperties.getObjectSizeLimit()))
-                .build();
-    }
 
     // used to fetch status lists with max memory size limit
     @Bean

722003b7555ee1195c50223d9257f54087a88c03
Karim Kummer <karim.kummer@bit.admin.ch>
2026-02-05 14:02:27 +0100

    fix(EIDOMNI-693): Fix DID resolving using didId and cache JWK instead of DidDoc

--- verifier-service/src/main/java/ch/admin/bj/swiyu/verifier/service/publickey/DidResolverFacade.java
+++ verifier-service/src/main/java/ch/admin/bj/swiyu/verifier/service/publickey/DidResolverFacade.java
@@ -9,7 +9,6 @@ package ch.admin.bj.swiyu.verifier.service.publickey;
 import ch.admin.bj.swiyu.didresolveradapter.DidResolverAdapter;
 import ch.admin.bj.swiyu.didresolveradapter.DidResolverException;
 import ch.admin.bj.swiyu.verifier.common.config.UrlRewriteProperties;
-import ch.admin.eid.didresolver.Did;
 import ch.admin.eid.did_sidekicks.DidDoc;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,7 +16,6 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 
-import static ch.admin.bj.swiyu.verifier.common.config.CachingConfig.ISSUER_PUBLIC_KEY_CACHE;
 import static ch.admin.bj.swiyu.verifier.common.config.CachingConfig.TRUST_STATEMENT_CACHE;
 
 
@@ -39,18 +37,11 @@ public class DidResolverFacade {
      * @param didId - the id of the DID Document
      * @return the DID Document for the given DID
      */
-    @Cacheable(ISSUER_PUBLIC_KEY_CACHE)
     public DidDoc resolveDid(String didId) throws DidResolverException {
-
         if (didId == null) {
             throw new IllegalArgumentException("did must not be null");
         }
-        try (var did = new Did(didId)) {
-            String didUrl = did.getUrl();
-            return didResolverAdapter.resolveDid(didUrl, urlRewriteProperties.getUrlMappings());
-        } catch (Exception e) {
-            throw new DidResolverException(e);
-        }
+        return didResolverAdapter.resolveDid(didId, urlRewriteProperties.getUrlMappings());
     }
 
     @Cacheable(TRUST_STATEMENT_CACHE)
